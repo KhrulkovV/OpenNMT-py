646d12687cad55723d23250a5e8567bce71a5115
Valentin Khrulkov <khrulkov.v@gmail.com>
2019-01-15 12:52:30 +0000

    script

--- scripts/prepare_all.sh
+++ scripts/prepare_all.sh
@@ -21,6 +21,7 @@ cd ../..
 
 
 mkdir data
+mkdir data/processed
 bash OpenNMT-py/scripts/prepare_data.sh raw_data
 
 python OpenNMT-py/preprocess.py -train_src data/train.en \
@@ -28,6 +29,6 @@ python OpenNMT-py/preprocess.py -train_src data/train.en \
     -valid_src data/valid.en -valid_tgt data/valid.de \ 
     -save_data data/processed/ \
     -src_seq_length 100 -tgt_seq_length 100 \ 
-    -max_shard_size 200000000 -share_vocab
+    -shard_size 200000000 -share_vocab
 
 mkdir model
---
+++ scripts/translate_bleu.sh
@@ -0,0 +1,15 @@
+MODEL=$(ls model/_step_* |sort -n -t _ -k 3 | tail -1)
+
+
+CUDA_VISIBLE_DEVICES=7 python OpenNMT-py/translate.py \
+				 -gpu 0 -replace_unk -alpha 0.6 -beta 0.0 \ 
+				-beam_size 5 -length_penalty wu -coverage_penalty wu \
+				-model $MODEL -src data/news-commentary-v11.de-en.en.sp \
+				—output data/prediction.txt -verbose
+
+
+spm_decode --model data/wmtende.model data/news-commentary-v11.de-en.de.sp --output data/news-commentary-v11.de-en.de.detok
+spm_decode --model data/wmtende.model data/prediction.txt --output data/prediction.detok
+
+
+perl OpenNMT-py/tools/multi-bleu-detok.perl data/news-commentary-v11.de-en.de.detok < data/prediction.detok
