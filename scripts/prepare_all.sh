5b356563fc160edce6d69ea12b3a788124176ef6
Valentin Khrulkov <khrulkov.v@gmail.com>
2019-01-15 07:17:40 +0000

    fix

--- scripts/prepare_all.sh
+++ scripts/prepare_all.sh
@@ -17,10 +17,10 @@ cd ../..
 mkdir data
 bash OpenNMT-py/scripts/prepare_data.sh raw_data
 
-python OpenNMT-py/preprocess.py -train_src data/train.en
-    -train_tgt data/train.de
-    -valid_src data/valid.en -valid_tgt data/valid.de 
-    -save_data data/processed/
-    -src_seq_length 100 -tgt_seq_length 100 
-    -max_shard_size 200000000 -share_vocab:q
+python OpenNMT-py/preprocess.py -train_src data/train.en \
+    -train_tgt data/train.de \
+    -valid_src data/valid.en -valid_tgt data/valid.de \ 
+    -save_data data/processed/ \
+    -src_seq_length 100 -tgt_seq_length 100 \ 
+    -max_shard_size 200000000 -share_vocab
 
