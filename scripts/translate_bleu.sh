84f93bd6af38381471e0b05f4823a3d5e3ad7a31
Valentin Khrulkov <khrulkov.v@gmail.com>
2019-01-15 13:07:06 +0000

    fix

--- scripts/translate_bleu.sh
+++ scripts/translate_bleu.sh
@@ -1,7 +1,8 @@
 MODEL=$(ls model/_step_* |sort -n -t _ -k 3 | tail -1)
 
 
-CUDA_VISIBLE_DEVICES=7 python OpenNMT-py/translate.py -gpu 0 -replace_unk -alpha 0.6 -beta 0.0 -beam_size 5 -length_penalty wu -coverage_penalty wu -model $MODEL -src data news-commentary-v11.de-en.en.sp —output data/prediction.txt -verbose
+CUDA_VISIBLE_DEVICES=7 python OpenNMT-py/translate.py -gpu 0 -replace_unk -alpha 0.6 -beta 0.0 -beam_size 5 -length_penalty wu -coverage_penalty wu -model $MODEL -src data/news-commentary-v11.de-en.en.sp —output data/prediction.txt -verbose
+
 
 
 spm_decode --model data/wmtende.model data/news-commentary-v11.de-en.de.sp --output data/news-commentary-v11.de-en.de.detok
